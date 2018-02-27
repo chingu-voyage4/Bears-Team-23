cf985793e8d6edd4b62ea486d0ba7ad7b63ce3f7
Dereje1 <Dereje1>
2018-02-27 16:16:11 -0500

    connect to db, add models and schemas for mongoose, add .env

---
+++ models/db.js
@@ -0,0 +1,34 @@
+"use strict"
+//creates one place for making the connection to the db
+// Bring Mongoose into the app
+const mongoose = require( 'mongoose' );
+
+// Build the connection string
+const dbURI = process.env.MONGOLAB_URI;
+// Create the database connection
+mongoose.connect(dbURI);
+//for previous mongoose versions use mongoose.connect(dbURI, { useMongoClient: true }) in case of conflict with express-sessions;
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', ()=> {
+  console.log('Mongoose default connection open to ' + dbURI);
+});
+
+// If the connection throws an error
+mongoose.connection.on('error',(err)=> {
+  console.log('Mongoose default connection error: ' + err);
+});
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', ()=> {
+  console.log('Mongoose default connection disconnected');
+});
+
+// If the Node process ends, close the Mongoose connection
+process.on('SIGINT', ()=>{
+  mongoose.connection.close(()=>{
+    console.log('Mongoose default connection disconnected through app termination');
+    process.exit(0);
+  });
+});
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const private = require('dotenv').config()
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,38 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+//establishes db connection
+const db = require('./models/db')
+//access schemas / collections
+const pictures = require('./models/schemas/pictures')
+
+//Testing database below
+app.post('/testdb',(req,res)=>{
+  const addedPic = req.body
+  pictures.create(addedPic,(err,pic)=>{
+    if(err){
+      throw err
+    }
+    else{
+      res.json(pic)
+    }
+  })
+})
+
+app.get('/testdb',(req,res)=>{
+  pictures.find({},(err,pics)=>{
+    if(err){
+      throw err
+    }
+    else{
+      res.json(pics)
+    }
+  })
+})
+
+//end db testing
+
+//other routing and testing below
 app.get('/guest', (req, res) => {
  let headerObject = req.headers
  //the x-forwarded-for property of the header does not appear for local host so add an alternative or will
@@ -21,18 +54,18 @@ app.get('/guest', (req, res) => {
    });
 });
 
-  app.get('/profile', (req, res) => {
-   let headerObject = req.headers
-   //the x-forwarded-for property of the header does not appear for local host so add an alternative or will
-   //error out locally on split to get the ip address the rest of the requests are common to loacl and remote
-   let ip = (headerObject['x-forwarded-for']||req.socket.remoteAddress).split(",")[0];
-   ip = (ip === "::1") ? "local" : ip
-      res.json({
-        authenticated: false,
-        userip: ip,
-        username: null,
-        displayname: null
-     });
-  });
+app.get('/profile', (req, res) => {
+ let headerObject = req.headers
+ //the x-forwarded-for property of the header does not appear for local host so add an alternative or will
+ //error out locally on split to get the ip address the rest of the requests are common to loacl and remote
+ let ip = (headerObject['x-forwarded-for']||req.socket.remoteAddress).split(",")[0];
+ ip = (ip === "::1") ? "local" : ip
+    res.json({
+      authenticated: false,
+      userip: ip,
+      username: null,
+      displayname: null
+   });
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
