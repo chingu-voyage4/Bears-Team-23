6f2a7bd3f87cb93910c072717ac80de853a674f8
Chris Mayer <mayerxc@gmail.com>
2018-03-04 10:39:31 -0500

    Create Upload.js and Upload.css files

---
+++ client/src/Components/Upload.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../css/Upload.css";
+
+
+const Upload = () => {
+    return (
+        <div>
+            <h1>Test</h1>
+        </div>
+    );
+}
+
+export default Upload;
