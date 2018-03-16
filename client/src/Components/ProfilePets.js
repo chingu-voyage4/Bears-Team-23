99c1e63613ec27e021c27d558665bb5811c10e52
Dereje1 <Dereje1>
2018-03-16 20:29:40 +0300

    client .env set, profile page css tweak + responiveness tweak

--- client/src/Components/ProfilePets.js
+++ client/src/Components/ProfilePets.js
@@ -8,9 +8,13 @@ const ProfilePets = (props) => {
     return (
         <div className = "ProfilePets">
             <p className = 'ProfilePets__name'>{props.petObject.petName}</p>
-            <img className = 'ProfilePets__img' src = {props.petObject.imgLink} alt = "" />
-            <p className = 'ProfilePets__rating'>Cuteness Rating: {props.petObject.avgRating}</p>
-            <button onClick={()=>props.deletePic(props.petObject._id)}> Delete </button>
+            <div className = 'ProfilePets__imgholder'>
+              <img className = 'ProfilePets__img' src = {props.petObject.imgLink} alt = "" />
+            </div>
+            <div className = 'ProfilePets__info'>
+              <p className = 'ProfilePets__rating'>Cuteness Rating: {props.petObject.avgRating}</p>
+              <button className = 'ProfilePets__delete' onClick={()=>props.deletePic(props.petObject._id)}> Delete </button>
+            </div>
         </div>
     )
 }
