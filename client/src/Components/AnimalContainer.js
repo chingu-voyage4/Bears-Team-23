f8ca8d225890be9ac03ed9530b25e333c705ebb7
kevin578 <kevinbriggs1@gmail.com>
2018-04-02 20:43:51 -0400

    added Cuteness Rating display after vote

--- client/src/Components/AnimalContainer.js
+++ client/src/Components/AnimalContainer.js
@@ -7,7 +7,8 @@ class AnimalContainer extends React.Component {
       super(props);
       this.state = {
         currentPic:null,
-        isLoading: false
+        isLoading: false,
+        showSkip: true
       };
       this.setRandomPic=this.setRandomPic.bind(this)
     }
@@ -21,15 +22,48 @@ class AnimalContainer extends React.Component {
       this.setState({
         isLoading: false,
         currentPic: randPic,
+        showSkip: true
       })
     }
     voteOnPic = async (value)=> {
-      this.setState({isLoading: true});
       const response = await updatePic(this.state.currentPic, value)
       if(response){
-        this.setRandomPic()
+        this.setState({
+          currentPic: response,
+          isLoading: false,
+          showSkip: false
+        });
+        setTimeout(()=>this.setRandomPic(), 1300)
+
       }
     }
+
+    getAnimalContainerIcons() {
+      if (this.state.currentPic.votable) {
+        return (
+        <div className = 'AnimalContainer__icons'>
+          <div onClick = {()=>this.voteOnPic(0)}><i className="fas fa-times-circle"></i></div>
+          <div onClick = {this.setRandomPic}><i className="fas fa-step-forward"></i></div>
+          <div onClick = {()=>this.voteOnPic(1)}><i className="fas fa-check-circle"></i></div>
+        </div>
+        )
+      }
+      else if(!this.state.currentPic.votable && this.state.showSkip) {
+        return (
+        <div className = 'AnimalContainer__icons'>
+          <p className = "AnimalContainer__icons__rating">Cuteness Rating: {Math.round(this.state.currentPic.avgRating * 100, 2)}%</p>
+          <div onClick = {this.setRandomPic}><i className="fas fa-step-forward skipButton"></i></div> 
+        </div>
+        )
+      }
+      else if (!this.state.currentPic.votable && !this.state.showSkip) {
+        return (
+        <p className = "AnimalContainer__icons__rating__after">Cuteness Rating: {Math.round(this.state.currentPic.avgRating * 100, 2)}%</p>
+        )
+      }
+    }
+
+
     render() {
       if(this.state.currentPic && !this.state.isLoading){
         return (
@@ -39,14 +73,8 @@ class AnimalContainer extends React.Component {
             className = "AnimalContainer__img"
             alt = ""
             onError={this.setRandomPic}/>
-            <p className = "AnimalContainer__petName">{this.state.currentPic.petName}</p>
-
-            <div className = 'AnimalContainer__icons'>
-              <div onClick = {()=>this.voteOnPic(0)}><i className="fas fa-times-circle"></i></div>
-              <div onClick = {this.setRandomPic}><i className="fas fa-step-forward"></i></div>
-              <div onClick = {()=>this.voteOnPic(1)}><i className="fas fa-check-circle"></i></div>
-            </div>
-
+          <p className = "AnimalContainer__petName">{this.state.currentPic.petName}</p>
+          {this.getAnimalContainerIcons()}
           </div>
         )
       }
