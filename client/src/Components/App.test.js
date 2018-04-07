13125ce0d7835f5dbc3f7cafdbdcbae99dd5ca2e
John Gillespie <jesgillespie@gmail.com>
2018-04-07 08:03:54 -0400

    updated tests/basic-smoke-tests

--- client/src/Components/App.test.js
+++ client/src/Components/App.test.js
@@ -1,8 +1,18 @@
-import React from 'react';
+
+
+import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
+import {userInfo} from './../crud/CRUD';
+import Navbar from "./Navbar";
+import Signup from "./Signup";
+import Footer from "./Footer";
+import AnimalContainer from './AnimalContainer';
+import "./../css/App.css";
 import App from './App';
 import { shallow } from 'enzyme';
 
 
 it('App renders without error', () => {
-  shallow(<App />);
+  shallow(<App />),
+  document.getElementById('root') || document.createElement('div');
 });
